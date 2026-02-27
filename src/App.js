743c413949c408461a4472b990e71f90c26ce5ca
LaPlace Sallis IV <laplace.sallis@gmail.com>
2026-02-26 19:02:48 -0600

    jndikns

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          yo lovely fat mama
+          yo loelt fat mama
         </p>
         <a
           className="App-link"
